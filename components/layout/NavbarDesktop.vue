8445bd0de99667e5c27504b47d08f85ec72613a1
x4e554c4c <wanderingdwarf03@gmail.com>
2021-02-17 22:21:41 +0200

    desktop navbar

---
+++ components/layout/NavbarDesktop.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class="navbar-desktop">
+        <div class="navbar-desktop__head">
+            <div class="navbar-desktop__profile"></div>
+        </div>
+
+      <ul class="navbar-desktop__links">
+        <li class="navbar-desktop__link">
+          type
+        </li>
+        <li class="navbar-desktop__link">
+          samples
+        </li>
+        <li class="navbar-desktop__link navbar-desktop__link--active">
+            leaders
+        </li>
+      </ul>
+    </nav>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,7 @@
 <template>
   <div class="layout-deafault">
 
-    <nav class="navbar-desktop">
-      <div class="navbar-desktop__head">
-        <div class="navbar-desktop__profile">
-
-        </div>
-      </div>
-
-      <ul class="navbar-desktop__links">
-        <li class="navbar-desktop__link">
-          some text
-        </li>
-        <li class="navbar-desktop__link">
-          some text
-        </li>
-        <li class="navbar-desktop__link navbar-desktop__link--active">
-          some text
-        </li>
-      </ul>
-    </nav>
+    <navbar-desktop/>
 
     <Nuxt />
   </div>
