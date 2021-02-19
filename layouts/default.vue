a83fdb1a39774e5adfd0d73bf8a30a844a266ad0
x4e554c4c <wanderingdwarf03@gmail.com>
2021-02-19 05:51:59 +0200

    resolution module and some flex at sass

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,3 +4,32 @@
     <Nuxt />
   </div>
 </template>
+
+<script>
+import {Observer as Resolution} from '@/services/Resolution'
+
+export default {
+  provide() {
+    return {
+      '$resolution': this.resolution
+    }
+  },
+
+  data() {
+    return {
+      resolution: null
+    }
+  },
+
+  watch: {
+    resolution(value) {
+      console.log(value);
+    }
+  },
+
+  mounted() {
+    let watcher = new Resolution();
+    watcher.bind(value => this.resolution = value);
+  }
+}
+</script>
---
+++ services/Resolution.ts
@@ -0,0 +1,96 @@
+import {throttle} from 'throttle-debounce'
+
+
+export interface Event {
+    [key: string]: boolean
+}
+
+export interface Listener {
+    (event: string) : number
+}
+
+interface Breakpoints {
+    [key: string] : number
+}
+
+export class Observer 
+{
+    private _listeners: Listener[] = []
+
+    private _current: string = '';
+    
+    private readonly delay: number = 300;
+
+    public readonly breakpoints: Breakpoints  = {
+        'mobile': 1200,
+        'laptop': 1920,
+        'desktop': Infinity
+    }
+
+    constructor() {
+        this.decide();
+
+        let throtteled: () => void = 
+            throttle(this.delay, this.decide.bind(this))
+        
+        window.addEventListener('resize', throtteled);
+    }
+
+    private set(resolution: string): void {
+        if (resolution === this._current)
+            return;
+
+        this._current = resolution;
+
+        for (let callback of this._listeners)
+            callback(this._current);
+    }
+
+    private decide(): void {
+        let width: number = window.innerWidth;
+
+        for (let name in this.breakpoints) {
+            let point = this.breakpoints[name];
+
+            if (width > point)
+                continue
+
+            this.set(name)
+            break;
+        }
+    }
+
+    public listen(callback: Listener): boolean {
+        let index: number = 
+            this._listeners.indexOf(callback);
+
+        if (index !== -1)
+            return false;
+
+        this._listeners.push(callback)
+        return true;
+    }
+
+    public detach(callback: Listener) {
+        let index: number = 
+            this._listeners.indexOf(callback);
+            
+        if (index === -1)
+            return false;
+
+        this._listeners.splice(index, 1)
+        return true;
+    }
+
+    public bind(callback: Listener): boolean {
+        if (!!!this.listen(callback))
+            return false;
+
+        callback(this._current);
+        return true;
+    }
+
+    public current(): string {
+        return this._current
+    }
+}
